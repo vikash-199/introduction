c5ed94ffaaeaee525dd5749b02aa000efed69e86
vikash kumar <vikashkumar24041997@gmail.com>
2025-03-01 06:28:31 +0530

    from validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import Card from "./shared/Card";
 function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
+  const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be at lest 10 characters");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(e.target.value);
+  };
   return (
     <Card>
       <form action="">
         <h2>How would you rate your service with us?</h2>
         <div className="input-group">
-          <input type="text" placeholder="Write a review" />
-          <button type="submit">Send</button>
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <button type="submit" disabled={btnDisabled}>
+            Send
+          </button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
