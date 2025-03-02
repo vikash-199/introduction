878f69cb3e9f1b4c3fc1e61ad3180e4d7b606a2b
vikash kumar <vikashkumar24041997@gmail.com>
2025-03-02 12:35:04 +0530

    adding newFeedback

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import Card from "./shared/Card";
+import RatingSelect from "./RatingSelect";
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
 
@@ -17,10 +19,18 @@ function FeedbackForm() {
     }
     setText(e.target.value);
   };
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      setMessage("Text must be at least 10 characters");
+      return;
+    }
+  }
   return (
     <Card>
-      <form action="">
+      <form onSubmit={handleSubmit} style={{ backgroundColor: "lightyellow" }}>
         <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -33,6 +43,7 @@ function FeedbackForm() {
           </button>
         </div>
         {message && <div className="message">{message}</div>}
+        <br />
       </form>
     </Card>
   );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,16 +1,18 @@
 import { FaTimes } from "react-icons/fa";
 import Card from "./shared/Card";
+import { LiaGhostSolid } from "react-icons/lia";
 
 function FeedbackItem({ item, handleDelete }) {
   return (
-    <Card>
+    <div style={{ backgroundColor: "lightblue" }}>
       <button onClick={() => handleDelete(item.id)}>
         {" "}
         <FaTimes color="blue" />
       </button>
       <div className="num-display">{item.rating}</div>
       <div className="text-disply">{item.text}</div>
-    </Card>
+      <br />
+    </div>
   );
 }
 
